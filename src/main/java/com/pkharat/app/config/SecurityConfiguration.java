b8b126f3d4f0916078c41ecba0ef1301e897426e
Purab <purabdk@yahoo.co.in>
2021-10-13 17:05:39 +0530

    Initial commit

---
+++ src/main/java/com/pkharat/app/config/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.pkharat.app.config;
+
+import com.pkharat.app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+}
